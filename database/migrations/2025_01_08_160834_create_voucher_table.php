6c723a852e3536925da5438ce907914f0dfa75a6
abduljabbar-punch <abduljabbaribrahim@punch.agency>
2025-01-08 17:16:45 +0100

    add consumptions and all

---
+++ database/migrations/2025_01_08_160834_create_voucher_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->string('voucher_number')->unique();
+            $table->foreignIdFor(\App\Models\Employee::class)->constrained();
+            $table->enum('status', ['pending', 'processing', 'completed', 'rejected'])->default('pending');
+            $table->text('purpose')->nullable();
+            $table->decimal('total_amount', 10, 2);
+            $table->foreignId('processed_by_id')->nullable()->constrained('users');
+            $table->timestamp('processed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('voucher');
+    }
+};
